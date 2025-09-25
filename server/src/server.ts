26394a690bfe39238b267f1168ba8f228885741d
ahmad <ahmadsaeed3290@gmail.com>
2025-09-25 07:34:51 +0500

    Getting response form spootify completed

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,15 +4,25 @@ import pool from "./config/db.js";
 import { Request, Response } from "express";
 import { createUsers } from "./controllers/createUsers.controller.js";
 import dotenv from "dotenv"
+import axios from "axios"
 
 dotenv.config()
 
 const app = express();
 app.use(express.json());
 
+interface User{
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+}
+
 const PORT = 5000;
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+
+let token: string;
 
 const authOptions = {
   url: 'https://accounts.spotify.com/api/token',
@@ -25,22 +35,26 @@ const authOptions = {
   json: true
 };
 
+
 request.post(authOptions, function(error: any, response: ReqResponse, body: { access_token: string }) {
   if (!error && response.statusCode === 200) {
-    const token = body.access_token;
-    console.log("Spotify token:", token);
+    token = body.access_token;
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   } else {
     console.error("Error fetching token:", error || body);
   }
 });
 
-
-interface User{
-  id: number;
-  username: string;
-  email: string;
-  password: string;
-}
+app.get("/search/:query", async(req:Request, res: Response) => {
+  const searchQuery = req.params.query;
+  try {
+    const spotifyResponse = await axios.get(`https://api.spotify.com/v1/search?q=${encodeURIComponent(searchQuery)}&type=artist,album,track`);
+    res.status(200).json(spotifyResponse.data);
+  }
+  catch(error:any){
+    res.status(500).json({success:false, message: (error as Error).message})
+  }
+})
 
 app.get("/users", async (req: Request, res: Response) => {
   try {
@@ -56,12 +70,3 @@ app.post("/create-account", createUsers)
 app.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}/`);
 });
-function generateRandomString(length: number): string {
-  let text = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
-
