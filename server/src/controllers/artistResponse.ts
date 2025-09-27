8e1237dab846a55009180105a69af83f82601c3a
ahmad <ahmadsaeed3290@gmail.com>
2025-09-27 10:43:23 +0500

    Separate controllers for artist,album,tracks. Refactored artist controller and table

---
+++ server/src/controllers/artistResponse.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import axios from "axios";
+import pool from "../config/db.js";
+
+interface AxiosResponse{
+  artists:{
+    items: {
+      name:string;
+      id: string;
+      images:{
+        url: string;
+      }[];
+    }[];
+  };
+}
+
+export const artistResponse = async(req:Request, res: Response) => {
+  let artistRow;
+  
+  const searchQuery = req.params.query;
+  
+  try {
+    const spotifyResponse = await axios.get<AxiosResponse>(`https://api.spotify.com/v1/search?q=${encodeURIComponent(searchQuery)}&type=artist`);
+    const startingQueryText = spotifyResponse.data.artists.items[0]
+    const artistName = startingQueryText.name
+    const artistId = startingQueryText.id
+    const artistImage = startingQueryText.images[0].url
+
+      const checkDBForArtist = await pool.query(`SELECT * FROM artists WHERE name = $1`, [artistName])
+
+      if (checkDBForArtist.rows.length === 0) {
+        const insertedRow = await pool.query(
+          'INSERT INTO artists(spotify_id, name, image) VALUES($1, $2, $3) RETURNING *',
+          [artistId, artistName, artistImage]
+        );
+        artistRow = insertedRow.rows[0]
+
+      }
+      else{
+        artistRow = checkDBForArtist.rows[0]
+      }
+    
+    res.json({id: artistRow.spotify_id, artistName:artistRow.name , artistImageURL: artistRow.image});
+  }
+  catch(error){
+    res.status(500).json({success:false, message: (error as Error).message})
+  }
+}
--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,10 +1,10 @@
 import express from "express"
-import { spotifyResponse } from "../controllers/spotifyResponse.controller.js"
+import { artistResponse } from "../controllers/artistResponse.js"
 import { createUsers } from "../controllers/createUsers.controller.js"
 
 export const router = express.Router()
 
 
-router.get("/search/:query", spotifyResponse)
+router.get("/search/:query", artistResponse)
 
 router.post("/create-account", createUsers)
