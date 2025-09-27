08b94d8d748fca4bf2d760e12cb4ccb219abce1e
ahmad <ahmadsaeed3290@gmail.com>
2025-09-27 13:10:15 +0500

    updates to artistResponse controller

--- server/src/controllers/artistResponse.ts
+++ server/src/controllers/artistResponse.ts
@@ -1,12 +1,14 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import axios from "axios";
 import pool from "../config/db.js";
+import { start } from "repl";
 
 interface AxiosResponse{
   artists:{
     items: {
       name:string;
       id: string;
+      genres: string[];
       images:{
         url: string;
       }[];
@@ -14,26 +16,24 @@ interface AxiosResponse{
   };
 }
 
-export const artistResponse = async(req:Request, res: Response, next: NextFunction) => {
+export const artistResponse = async(req:Request, res: Response) => {
   let artistRow;
   
   const searchQuery = req.params.query;
   
   try {
     const spotifyResponse = await axios.get<AxiosResponse>(`https://api.spotify.com/v1/search?q=${encodeURIComponent(searchQuery)}&type=artist`);
-    if (!spotifyResponse.data.artists?.items?.length) return next();
-
     const startingQueryText = spotifyResponse.data.artists.items[0]
     const artistName = startingQueryText.name
     const artistId = startingQueryText.id
     const artistImage = startingQueryText.images[0].url
-
+    const artistGenre = startingQueryText.genres[0]
     const checkDBForArtist = await pool.query(`SELECT * FROM artists WHERE name = $1`, [artistName])
 
     if (checkDBForArtist.rows.length === 0) {
         const insertedRow = await pool.query(
-          'INSERT INTO artists(spotify_id, name, image) VALUES($1, $2, $3) RETURNING *',
-          [artistId, artistName, artistImage]
+          'INSERT INTO artists(spotify_id, name, image, genre) VALUES($1, $2, $3, $4) RETURNING *',
+          [artistId, artistName, artistImage, artistGenre]
         );
         artistRow = insertedRow.rows[0]
 
@@ -42,7 +42,7 @@ export const artistResponse = async(req:Request, res: Response, next: NextFuncti
         artistRow = checkDBForArtist.rows[0]
       }
     
-    res.json({id: artistRow.spotify_id, artistName:artistRow.name , artistImageURL: artistRow.image});
+    res.json({id: artistRow.spotify_id, artistName:artistRow.name , artistImageURL: artistRow.image, artistGenre: artistRow.genre});
   }
   catch(error){
     res.status(500).json({success:false, message: (error as Error).message})
