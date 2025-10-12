0ede4103457fcba3ed22b81647594921e5f4f08c
ahmad <ahmadsaeed3290@gmail.com>
2025-10-12 22:58:04 +0500

    Update: Navigate to artist page and show albums and info

--- client/src/Pages/AristPage.tsx
+++ client/src/Pages/AristPage.tsx
@@ -1,9 +1,12 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import { useParams } from "react-router-dom"
 
 export const ArtistPage: React.FC = () =>{
-    const query = "kanye"
+        const { query } = useParams()
+        const effect = useRef(false)
         useEffect(()=>{
+            if (effect.current) return
+            effect.current = true
            async function fetchAlbums(){
                 const res = await fetch(`http://localhost:5000/artist/${encodeURIComponent(query)}`)
                 const data = await res.json()
