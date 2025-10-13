ebf6767f23a1b8d9197e2fd024e9b37e453e9a8e
ahmad <ahmadsaeed3290@gmail.com>
2025-10-13 23:06:21 +0500

    Update: Show albums in a list

--- client/src/Pages/AristPage.tsx
+++ client/src/Pages/AristPage.tsx
@@ -1,23 +1,36 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export const ArtistPage: React.FC = () =>{
         const { query } = useParams()
         const effect = useRef(false)
+        const [artistAlbums, setArtistAlbums] = useState([])
         useEffect(()=>{
             if (effect.current) return
             effect.current = true
            async function fetchAlbums(){
                 const res = await fetch(`http://localhost:5000/artist/${encodeURIComponent(query)}`)
                 const data = await res.json()
-                console.log(data)
+                setArtistAlbums(data)
             }
             fetchAlbums()
         }, [])
 
             return(
                 <>
-                    <p>artist page type sht</p> 
+             <div className="flex flex-row overflow-x-auto space-x-4 p-2">
+  {artistAlbums.map((album) => (
+    <div key={album?.id} className="flex-shrink-0">
+      <img
+        src={album?.image}
+        alt={album?.name}
+        className="w-32 h-32 object-cover rounded-md"
+      />
+      <p className="mt-2 text-center text-sm font-medium">{album?.name}</p>
+    </div>
+  ))}
+</div>
+
                 </>
             )   
 }
