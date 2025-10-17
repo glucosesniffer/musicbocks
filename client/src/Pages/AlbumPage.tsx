6efc3d1bc5a130af3dfc2cfba32f3945f238de47
ahmad <ahmadsaeed3290@gmail.com>
2025-10-17 06:23:07 +0500

    Feat: Added Stars UI

--- client/src/Pages/AlbumPage.tsx
+++ client/src/Pages/AlbumPage.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export const AlbumPage: React.FC = () => {
   const { id } = useParams();
   const [albumInfo, setInfo] = useState();
+  const [rating, setRating] = useState(0);
   const effect = useRef(false);
+  const [session, setSession] = useState(null);
   useEffect(() => {
     if (effect.current) return;
     effect.current = true;
@@ -14,18 +17,84 @@ export const AlbumPage: React.FC = () => {
       console.log(data);
       setInfo(data);
     };
-    fetchAlbumfromId();
-  }, [id]);
 
+    fetchAlbumfromId();
+    const sessionid = localStorage.getItem("sessionId");
+    setSession(sessionid);
+  }, [id]);
+  function handleCurrentSession() {
+    if (!session) console.log("login first");
+    return;
+  }
+  const artist_id = albumInfo?.artist_id;
+  const album_id = albumInfo?.id;
+  const addRating = async () => {
+    await axios.post("http://localhost:5000/review", {
+      rating,
+      artist_id,
+      album_id,
+    });
+  };
+  // console.log(rating);
   return (
     <div className="flex flex-row justify-evenly items-center w-full">
       <div className="">
         <img src={albumInfo?.image} className="rounded-md w-[250px]"></img>
       </div>
       <div className="flex flex-col items-center">
-        <h1 className="mt-2 font-bold max-w-[500px]">{albumInfo?.title}</h1>
+        <p className="mt-2 font-bold max-w-[500px] font-bold !text-4xl">
+          {albumInfo?.title}
+        </p>
         <p className="mt-2 text-zinc-400 text-s">Album</p>
         <p className="text-xs text-zinc-500 mt-1">{albumInfo?.year}</p>
+        <div className="rating rating-xl" onClick={handleCurrentSession}>
+          <input
+            type="radio"
+            name="rating-1"
+            className="mask mask-star bg-blue-500"
+            aria-label="1 star"
+            value="1"
+            disabled={!session}
+            onChange={(e: any) => setRating(e.target.value)}
+          />
+          <input
+            type="radio"
+            name="rating-1"
+            className="mask mask-star bg-blue-500"
+            aria-label="2 star"
+            value="2"
+            disabled={!session}
+            onChange={(e: any) => setRating(e.target.value)}
+          />
+          <input
+            type="radio"
+            name="rating-1"
+            className="mask mask-star bg-blue-500"
+            aria-label="3 star"
+            disabled={!session}
+            value="3"
+            onChange={(e: any) => setRating(e.target.value)}
+          />
+          <input
+            type="radio"
+            name="rating-1"
+            className="mask mask-star bg-blue-500"
+            aria-label="4 star"
+            disabled={!session}
+            value="4"
+            onChange={(e: any) => setRating(e.target.value)}
+          />
+
+          <input
+            type="radio"
+            name="rating-1"
+            className="mask mask-star bg-blue-500"
+            aria-label="5 star"
+            disabled={!session}
+            value="5"
+            onChange={(e: any) => setRating(e.target.value)}
+          />
+        </div>
       </div>
     </div>
   );
--- server/src/controllers/add-reviews.controller.ts
+++ server/src/controllers/add-reviews.controller.ts
@@ -1,21 +1,31 @@
 import { Request, Response } from "express";
 import pool from "../config/db.js";
 
-export const addReviews = async(req: Request, res: Response): Promise<void> => {
-    const {review, rating, album_id, user_id}: {review: string, rating: number, album_id: number, user_id: number} = req.body
+export const addReviews = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const {
+    rating,
+    album_id,
+    user_id,
+  }: { review: string; rating: number; album_id: number; user_id: number } =
+    req.body;
 
-    try{
-        if(req.session.userId){
-        const result = await pool.query(`INSERT INTO reviews (review, rating, album_id, user_id) 
-                                         VALUES ($1, $2, $3, $4) RETURNING *`, [review, rating, album_id, user_id])
-        res.status(200).json({success:true, data: result.rows})
-        }
-        else{
-            res.status(400).json({success:false, message: "Please login to review!"})
-        }
+  try {
+    if (req.session.userId) {
+      const result = await pool.query(
+        `INSERT INTO reviews (review, album_id, user_id) 
+                                         VALUES ($1, $2, $3, $4) RETURNING *`,
+        [rating, album_id, user_id]
+      );
+      res.status(200).json({ success: true, data: result.rows });
+    } else {
+      res
+        .status(400)
+        .json({ success: false, message: "Please login to review!" });
     }
-    catch(e){
-        res.status(400).json({success:false, message: e})
-    }
-
-}
+  } catch (e: any) {
+    res.status(400).json({ success: false, message: e.message });
+  }
+};
